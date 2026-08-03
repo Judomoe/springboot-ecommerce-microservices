8e397bdc10d685c73b2fe72a70639b8efc61d380
Judomoe <serafraf2005@gmail.com>
2026-08-04 00:05:20 +0300

    - added eureka to service to my project to let the microservices know each others locations -and made an api gateway so the client doesnt have to access each microservice using its url manually but just using the api gateway that then uses eureka to map them

---
+++ eureka-server/src/test/java/com/hamada/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamada.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
